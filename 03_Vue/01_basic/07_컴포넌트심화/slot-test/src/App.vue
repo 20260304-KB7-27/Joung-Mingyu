a2f80adbd5f0e2de62a454d1396dbcdfc92b0ff4
Mingyu Joung <samu9nai@gmail.com>
2026-03-25 15:30:07 +0900

    260325 실습

---
+++ 03_Vue/01_basic/07_컴포넌트심화/slot-test/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import NoSlotTest from './components/example1/NoSlotTest.vue';
+import SlotTest from './components/example1/SlotTest.vue';
+SlotTest;
+NoSlotTest;
+</script>
+
+<template>
+  <!-- <NoSlotTest /> -->
+  <SlotTest />
+</template>
+
+<style scoped></style>
---
+++ 03_Vue/01_basic/07_컴포넌트심화/style-test/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import './assets/main.css'; // 전역 설정
+
+createApp(App).mount('#app');
