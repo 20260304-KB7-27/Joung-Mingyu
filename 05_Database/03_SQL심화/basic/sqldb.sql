4f984f5fd7ce33c21edb557c2c600824ead52b92
Mingyu Joung <samu9nai@gmail.com>
2026-05-27 16:56:31 +0900

    260527 실습

---
+++ 05_Database/03_SQL심화/basic/sqldb.sql
@@ -0,0 +1,37 @@
+use sqldb;
+
+# sqldb 데이터베이스에서 다음 조건을 처리하세요
+# 사용자별로 구매 이력을 출력
+# 모든 컬럼을 출력
+# 구매 이력이 없는 정보는 출력하지 않음
+select * from buytbl b
+join usertbl u on b.userID = u.userID;
+
+# 앞의 결과에서 userID가 'JYP'인 데이터만 출력
+select  * from buytbl b
+join usertbl u on b.userID = u.userID
+where u.userID = 'JYP';
+
+# sqldb 데이터베이스에서 다음 조건을 처리하세요
+# 각 사용자별로 구매 이력을 출력
+# 연결 컬럼은 userID
+# 결과를 userID를 기준으로 오름차순 정렬
+# 구매이력이 없는 사용자도 출력
+# userID, name, prodName, addr, 연락처를 다음과 같이 출력
+select
+    u.userID,
+    u.name,
+    b.prodName,
+    u.addr,
+    concat(u.mobile1, u.mobile2) as 연락처
+from usertbl u
+left join buytbl b on b.userID = u.userID
+order by u.userID asc;
+
+# sqldb의 사용자를 모두 조회하되 전화가 없는 사람은 제외하고 출력
+select * from usertbl
+where mobile1 is not null;
+
+# sqldb의 사용자를 모두 조회하되 전화가 없는 사람만 출력
+select * from usertbl
+where mobile1 is null;
