6639b9250a1d96b9314bbf1540ca4d60cad5bf64
Mingyu Joung <samu9nai@gmail.com>
2026-06-24 17:35:22 +0900

    260624 실습

--- 07_Spring/aopex/src/main/java/org/scoula/config/ServletConfig.java
+++ 07_Spring/aopex/src/main/java/org/scoula/config/ServletConfig.java
@@ -15,22 +15,14 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableWebMvc
 @ComponentScan(basePackages = {
         "org.scoula.controller",
-        "org.scoula.board.controller",
-        "org.scoula.exception",
-        "org.scoula.weather.controller"})
+        "org.scoula.exception"})
+@Configuration
 public class ServletConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**")
+        registry
+                .addResourceHandler("/resources/**")
                 .addResourceLocations("/resources/");
-        registry.addResourceHandler("/swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-        registry.addResourceHandler("/swagger-resources/**")
-                .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/v2/api-docs")
-                .addResourceLocations("classpath:/META-INF/resources/");
     }
 
     @Override
